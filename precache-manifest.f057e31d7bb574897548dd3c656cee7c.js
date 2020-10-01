073bce10442e47889be3d7042b821690c5dd7100
Anastasiia Lukina <anlukina24@gmail.com>
2020-10-01 12:37:39 +0300

    Updates

---
+++ precache-manifest.f057e31d7bb574897548dd3c656cee7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a7d2b4b63450d61d0233ea1e69fe699",
+    "url": "/redux-TT/index.html"
+  },
+  {
+    "revision": "48541f2129d3b17e224a",
+    "url": "/redux-TT/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6bd2e70a9a2a1bde96c",
+    "url": "/redux-TT/static/js/2.114f7dd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/redux-TT/static/js/2.114f7dd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48541f2129d3b17e224a",
+    "url": "/redux-TT/static/js/main.059d26a2.chunk.js"
+  },
+  {
+    "revision": "58b95a366bf0bc5b0ad0",
+    "url": "/redux-TT/static/js/runtime-main.46af711d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/redux-TT/static/media/logo.5d5d9eef.svg"
+  }
+]);
